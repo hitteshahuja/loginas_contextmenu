9bcd19f02eee5e1f8faae2a6e61cf2d36b000c87
Hittesh Ahuja <hitteshahuja@gmail.com>
2013-09-03 11:23:04 +0100

    Changed checkbox parameter

--- settings.php
+++ settings.php
@@ -3,12 +3,12 @@ defined('MOODLE_INTERNAL') || die();
 require_once(dirname(__FILE__) . '/lib.php');
 if ($hassiteconfig) {
 	
-	$page = new admin_settingpage('loginas', get_string('pluginname', 'local_loginas_contextmenu'));
+	$page = new admin_settingpage('loginas_contextmenu', get_string('pluginname', 'local_loginas_contextmenu'));
 	$page->add(new admin_setting_heading('local_loginas_contextmenu/settingheading', 'Login As Settings', ''));
 	$page->add(new admin_setting_configcheckbox('local_loginas_contextmenu/enableloginas', get_string('enableloginas', 'local_loginas_contextmenu'),
-        get_string('enableloginasdesc', 'local_loginas_contextmenu'), array(), 1,0));
+        get_string('enableloginasdesc', 'local_loginas_contextmenu'),'', 1,0));
 	$choices = array('participants'=>'Participants page','enrolledusers'=>'Enrolled Users page');	
-	$page->add(new admin_setting_configmulticheckbox('local_loginas_contextmenu/loginaslocations',get_string('loginaslocations','local_loginas'),get_string('loginaslocationsdesc','local_loginas_contextmenu'),array(),$choices));
+	$page->add(new admin_setting_configmulticheckbox('local_loginas_contextmenu/loginaslocations',get_string('loginaslocations','local_loginas_contextmenu'),get_string('loginaslocationsdesc','local_loginas_contextmenu'),array(),$choices));
 	$ADMIN->add('localplugins', $page);
 	
 }
